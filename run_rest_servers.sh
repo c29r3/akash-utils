16dac55704536f599e4b9014d3eb12c1daccef47
Sergey <52304443+c29r3@users.noreply.github.com>
2020-05-14 16:30:17 +0300

    Update run_rest_servers.sh

--- run_rest_servers.sh
+++ run_rest_servers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmux new-session -d -s 1317 'akashctl rest-server --laddr --max-open 30000'
+tmux new-session -d -s 1317 'akashctl rest-server --max-open 30000'
 
 tmux new-session -d -s 1318 'akashctl rest-server --laddr tcp://localhost:1318 --node tcp://139.178.89.117:26657 --trust-node --max-open 30000'
 
