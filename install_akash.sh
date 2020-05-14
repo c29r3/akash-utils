1bc1301278ce27262d2e5c5eada4ff61e586c4bf
Sergey <52304443+c29r3@users.noreply.github.com>
2020-05-14 08:35:58 +0300

    Create install_akash.sh

---
+++ install_akash.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+apt install -y git jq
+cd /tmp/ && \
+wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz && \
+tar -C /usr/local -xzf go*.tar.gz;
+echo '\nexport PATH=/root/go/bin:$PATH
+export GOROOT=/usr/local/go
+export GOPATH=/root/go
+export GO111MODULE=on 
+export PATH=/root/go/bin:/root/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin:/root
+export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"\n' >> /root/.profile;
+. /root/.profile
+
+cd ~
+git clone https://github.com/ovrclk/akash.git && \
+cd akash && \
+git checkout v0.6.1 && \
+make deps-install && \
+make install
