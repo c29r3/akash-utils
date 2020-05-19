c4c19a02d7a70539c416bedc55db3aa04cec3dbb
Sergey <52304443+c29r3@users.noreply.github.com>
2020-05-20 01:49:52 +0300

    Update install_akash.sh

--- install_akash.sh
+++ install_akash.sh
@@ -4,12 +4,12 @@ apt install -y git jq
 cd /tmp/ && \
 wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz && \
 tar -C /usr/local -xzf go*.tar.gz;
-echo  'export PATH=/root/go/bin:$PATH
+echo -e '\nexport PATH=/root/go/bin:$PATH
 export GOROOT=/usr/local/go
 export GOPATH=/root/go
 export GO111MODULE=on 
 export PATH=/root/go/bin:/root/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/usr/local/go/bin:/usr/local/go/bin:/root
-export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"' >> /root/.profile;
+export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"\n' >> /root/.profile;
 . /root/.profile
 
 cd ~
