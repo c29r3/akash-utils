c1c8ce3a3c4a772989b7e377cdd2129c2959b592
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-07 04:35:46 +0300

    Create unjail.sh

---
+++ unjail.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WALLET_NAME=akash
+CHAIN_ID="akash-edgenet-1"
+CLI="/usr/local/bin/akash"
+RPC="http://localhost:29657"
+COIN="uakt"
+PWD=$1
+SELF_ADDR=$(echo -e "$PWD\n" | akash keys list | grep "address:" | awk '{print $2}')
+
+while true; 
+do 
+    OPERATOR=$($CLI q staking delegations -o json --node $RPC --chain-id $CHAIN_ID $SELF_ADDR | jq -r .delegation_responses[0].delegation.validator_address)
+    STATUS=$($CLI query staking validator $OPERATOR --chain-id=$CHAIN_ID --node $RPC -o json | jq -r .jailed)
+    echo "Status $STATUS"
+    if [[ "$STATUS" == "true" ]]; then
+        echo "UNJAIL"
+        echo -e "$PWD\n" | $CLI tx slashing unjail --from $WALLET_NAME --node $RPC --gas-adjustment="1.5" --gas="200000" --fees 5000$COIN --chain-id=$CHAIN_ID -y
+    fi
+    sleep 300
+done
