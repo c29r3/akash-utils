06aa3cb40a1159c174ef2d7bf9d04e46144e7882
Sergey <52304443+c29r3@users.noreply.github.com>
2020-12-31 16:53:55 +0300

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -2,26 +2,33 @@
 
 SELF_ADDR="akash..."
 OPERATOR="akashvaloper..."
-WALLET_NAME="teamname"
-WALLET_PWD="pwd"
-BIN_FILE="/root/go/bin/akashctl"
+WALLET_NAME="akash"
+CHAIN_ID="akashnet-1"
+WALLET_PWD="PASSWD"
+BIN_FILE="/usr/local/bin/akashctl"
+TOKEN="uakt"
 
-# withdraw reward
-echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx distribution withdraw-rewards $OPERATOR --commission --chain-id centauri --from $WALLET_NAME -y
-sleep 5
 
-# check current balance
-BALANCE=$($BIN_FILE query account $SELF_ADDR -o json | jq -r .value.coins[0].amount)
-echo CURRENT BALANCE IS: $BALANCE
+while true; do
+    # withdraw reward
+    echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx distribution withdraw-rewards $OPERATOR --commission --fees 1000$TOKEN --chain-id $CHAIN_ID --from $WALLET_NAME -y
 
-REWARD=$(( $BALANCE - 100000 ))
+    sleep 10
 
-if (( $BALANCE >=  2 ));then
-    echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
-    # delegate balance
-    echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx staking delegate $OPERATOR "$REWARD"uakt --chain-id centauri --from $WALLET_NAME -y
+    # check current balance
+    BALANCE=$($BIN_FILE query account $SELF_ADDR -o json | jq -r .value.coins[0].amount)
+    echo CURRENT BALANCE IS: $BALANCE
 
-else
-    echo "Reward is $REWARD"
-fi
-echo "DONE"
+    RESTAKE_AMOUNT=$(( $BALANCE - 50000000 ))
+
+    if (( $RESTAKE_AMOUNT >=  25000000 ));then
+        echo "Let's delegate $RESTAKE_AMOUNT of REWARD tokens to $SELF_ADDR"
+        # delegate balance
+        echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx staking delegate $OPERATOR "$RESTAKE_AMOUNT"$TOKEN --fees 1000$TOKEN --chain-id $CHAIN_ID --from $WALLET_NAME -y
+
+    else
+        echo "Reward is $RESTAKE_AMOUNT"
+    fi
+    echo "DONE"
+    sleep 86400
+done
