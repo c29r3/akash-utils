30e2dcac6121ee469f5dfcbeddc969b2d06a560b
Sergey <52304443+c29r3@users.noreply.github.com>
2021-02-01 03:49:13 +0300

    Update unjail.sh

--- unjail.sh
+++ unjail.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 WALLET_NAME=akash
-CHAIN_ID="akash-edgenet-1"
-CLI="/usr/local/bin/akash"
+CHAIN_ID="akashnet-1"
+CLI="/usr/local/bin/akashctl"
 RPC="http://localhost:26657"
 COIN="uakt"
-PSWD=$1
-SELF_ADDR=$(echo -e "$PSWD\n" | akash keys list | grep "address:" | awk '{print $2}')
+TG_TOKEN=""
+CHAT_ID="11111111"
+PASSWD="PSWD"
+SUBJECT="AKASH"
+
+SELF_ADDR=$(echo -e "$PASSWD\n" | akashctl keys list -o json | jq -r .[0].address)
 
 while true; 
 do 
-    OPERATOR=$($CLI q staking delegations -o json --node $RPC --chain-id $CHAIN_ID $SELF_ADDR | jq -r .delegation_responses[0].delegation.validator_address)
-    STATUS=$($CLI query staking validator $OPERATOR --chain-id=$CHAIN_ID --node $RPC -o json | jq -r .jailed)
+    OPERATOR=$($CLI q staking delegations -o json --node $RPC --chain-id $CHAIN_ID $SELF_ADDR | jq -r .[0].validator_address)
+    STATUS=$($CLI query staking validator $OPERATOR --chain-id=$CHAIN_ID --node $RPC -o json --trust-node | jq -r .jailed)
     echo "Status $STATUS"
     if [[ "$STATUS" == "true" ]]; then
         echo "UNJAIL"
-        echo -e "$PSWD\n" | $CLI tx slashing unjail --from $WALLET_NAME --node $RPC --gas-adjustment="1.5" --gas="200000" --fees 5000$COIN --chain-id=$CHAIN_>
+        if [[ $TG_TOKEN != "" ]]
+        then
+            MSG="Validator status = $STATUS"
+            $(which curl) -s -H 'Content-Type: application/json' --request 'POST' -d "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"${SUBJECT}\n${MSG}\"}" "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+        fi
+        echo -e "$PASSWD\n$PASSWD\n" | $CLI tx slashing unjail --from $WALLET_NAME --node $RPC --gas-adjustment="1.5" --gas="200000" --fees 5000$COIN --chain-id=$CHAIN_ID -y
     fi
     sleep 300
 done
