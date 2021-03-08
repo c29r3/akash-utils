82f850a081ce6c192ec17f2a758110f5ab66cc27
Sergey <52304443+c29r3@users.noreply.github.com>
2021-03-08 20:49:29 +0300

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SELF_ADDR="akash..."
-OPERATOR="akashvaloper..."
+SELF_ADDR="WALLET_ADDRESS"
+OPERATOR="OPERATOR_ADDRESS"
 WALLET_NAME="akash"
-CHAIN_ID="akashnet-1"
-WALLET_PWD="PASSWD"
-BIN_FILE="/usr/local/bin/akashctl"
+CHAIN_ID="akashnet-2"
+WALLET_PWD="PASSWORD"
+BIN_FILE="/usr/local/bin/akash"
 TOKEN="uakt"
 
 
@@ -16,19 +16,19 @@ while true; do
     sleep 10
 
     # check current balance
-    BALANCE=$($BIN_FILE query account $SELF_ADDR -o json | jq -r .value.coins[0].amount)
+    BALANCE=$($BIN_FILE q bank balances $SELF_ADDR -o json | jq -r .balances[0].amount)
     echo CURRENT BALANCE IS: $BALANCE
 
-    RESTAKE_AMOUNT=$(( $BALANCE - 50000000 ))
+    RESTAKE_AMOUNT=$(( $BALANCE - 5000000 ))
 
     if (( $RESTAKE_AMOUNT >=  25000000 ));then
         echo "Let's delegate $RESTAKE_AMOUNT of REWARD tokens to $SELF_ADDR"
         # delegate balance
-        echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx staking delegate $OPERATOR "$RESTAKE_AMOUNT"$TOKEN --fees 1000$TOKEN --chain-id $CHAIN_ID --from $WALLET_NAME -y
+        echo -e "$WALLET_PWD\n$WALLET_PWD\n" | $BIN_FILE tx staking delegate $OPERATOR "$RESTAKE_AMOUNT"$TOKEN --fees 3000$TOKEN --chain-id $CHAIN_ID --from $WALLET_NAME -y
 
     else
         echo "Reward is $RESTAKE_AMOUNT"
     fi
     echo "DONE"
-    sleep 86400
+    sleep 10800
 done
