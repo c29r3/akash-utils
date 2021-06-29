42a2a72434d3b7e54ae5cbbdb37efc08a285cce0
Sergey <52304443+c29r3@users.noreply.github.com>
2021-06-29 18:39:31 +0300

    Update install_akash.sh

--- install_akash.sh
+++ install_akash.sh
@@ -1,19 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
-apt install -y git jq
-cd /tmp/ && \
-wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz && \
-tar -C /usr/local -xzf go*.tar.gz;
-echo -e '\nexport PATH=/root/go/bin:$PATH
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export GO111MODULE=on
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/.bashrc;
-. $HOME/.bashrc
+BINARY_VERSION=$(curl -s https://raw.githubusercontent.com/ovrclk/net/master/mainnet/version.txt)
+DEB_FILE="https://github.com/ovrclk/akash/releases/download/v$BINARY_VERSION/akash_0.12.1_linux_amd64.deb"
+BIN_PATH="/usr/local/bin/akash"
+CHAIN_ID=$(curl -s https://raw.githubusercontent.com/ovrclk/net/master/mainnet/chain-id.txt)
+SERVICE_FILE="https://raw.githubusercontent.com/c29r3/akash-utils/master/akash.service"
+CONFIG_TOML="https://raw.githubusercontent.com/c29r3/akash-utils/master/config.toml"
+APP_TOML="https://raw.githubusercontent.com/c29r3/akash-utils/master/app.toml"
+GENESIS_URL="https://raw.githubusercontent.com/ovrclk/net/master/mainnet/genesis.json"
 
-cd ~
-git clone https://github.com/ovrclk/akash.git && \
-cd akash && \
-git checkout v0.7.4 && \
-make deps-install && \
-make install
+# install requirements
+sudo apt update
+sudo apt-get install -y jq curl wget htop
+
+# install binary
+cd /tmp
+wget $DEB_FILE &>/dev/null
+dpkg -i akash*deb
+$BIN_PATH version
+
+# init node
+$BIN_PATH init c29r3 --chain-id $CHAIN_ID
+
+# download configs
+curl -s $CHAIN_ID > $HOME/.akash/config/genesis.json
+curl -s $CONFIG_TOML > $HOME/.akash/config/config.toml
+curl -s $APP_TOML > $HOME/.akash/config/app.toml
+
+# install service unit
+curl -s $SERVICE_FILE > /etc/systemd/system/akash.service
+sudo systemctl daemon-reload
+sudo systemctl enable akash.service
+
+# download snapshot
+rm -rf ~/.akash/data
+mkdir -p ~/.akash/data
+cd ~/.akash/data
+# random wait
+sleep $(shuf -i 10-300 -n 1)
+SNAP_NAME=$(curl -s http://135.181.60.250/akash/ | egrep -o ">$CHAIN_ID.*tar" | tr -d ">"); \
+wget -O - http://135.181.60.250/akash/${SNAP_NAME} | tar xf -
+
+# start service
+systemctl start akash.service
