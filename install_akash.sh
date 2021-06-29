23a814fb9efbb2276b89bfd9ace7cde2581cb01e
Sergey <52304443+c29r3@users.noreply.github.com>
2021-06-29 18:56:45 +0300

    Update install_akash.sh

--- install_akash.sh
+++ install_akash.sh
@@ -19,33 +19,34 @@ wget $DEB_FILE &>/dev/null
 dpkg -i akash*deb
 $BIN_PATH version
 
-# init node
+echo "init node"
 $BIN_PATH init c29r3 --chain-id $CHAIN_ID
 
-# download configs
+echo "download configs"
 curl -s $CHAIN_ID > $HOME/.akash/config/genesis.json
 curl -s $CONFIG_TOML > $HOME/.akash/config/config.toml
 curl -s $APP_TOML > $HOME/.akash/config/app.toml
 
-# install service unit
+echo "install service unit"
 curl -s $SERVICE_FILE > /etc/systemd/system/akash.service
 sudo systemctl daemon-reload
 sudo systemctl enable akash.service
 
-# download snapshot
+echo "download snapshot"
 rm -rf ~/.akash/data
 mkdir -p ~/.akash/data
 cd ~/.akash/data
-# random wait
+
+echo "random wait"
 SLEEP_TIME=$(shuf -i 10-300 -n 1)
 echo $SLEEP_TIME
 sleep $SLEEP_TIME
 SNAP_NAME=$(curl -s http://135.181.60.250/akash/ | egrep -o ">$CHAIN_ID.*tar" | tr -d ">"); \
 wget -O - http://135.181.60.250/akash/${SNAP_NAME} | tar xf -
 
-# ufw rules
+echo "ufw rules"
 sudo ufw allow 28957,28959,28956,1518,9890/tcp comment "allow akash public nodes"
 
-# start service
+echo "start service"
 sudo systemctl start akash.service
 
