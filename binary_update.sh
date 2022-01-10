8a90161f68d37ee3c5adcedbb42d103188cac9c9
Sergey <52304443+c29r3@users.noreply.github.com>
2022-01-10 08:23:01 +0300

    Create binary_update.sh

---
+++ binary_update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+URL=https://github.com/ovrclk/akash/releases/download/v0.14.1/akash_0.14.1_linux_amd64.zip
+BIN_PATH=$HOME/go/bin/
+BIN_NAME=akash
+
+sudo systemctl stop ${BIN_NAME}
+
+sudo apt-get update && sudo apt-get install -y unzip
+
+cd ${BIN_PATH}
+
+rm ${BIN_NAME}
+
+wget ${URL}
+
+unzip *zip
+
+mv ${BIN_NAME}_*/akash .
+
+chmod 700 akash
+
+rm -rf ${BIN_NAME}_*
+
+sudo systemctl start ${BIN_NAME}
+
+sudo systemctl status ${BIN_NAME}
